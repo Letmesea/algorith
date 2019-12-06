43f03235f6ea8343cffd73ccfcb2e9a1ed90387f
LIANGZHONGHAI3 <LIANGZHONGHAI3@ceair.com>
2019-12-06 13:32:45 +0800

    打家劫舍

--- src/base/dynamicprogram/RobMoney.java
+++ src/base/dynamicprogram/RobMoney.java
@@ -27,44 +27,24 @@ import java.util.*;
  */
 public class RobMoney {
     public static void main(String[] args){
-
+        RobMoney robMoney= new RobMoney();
+        int a[]={2,1,1,2};
+        int res = robMoney.rob(a);
     }
+    public int rob1(int[] nums) {
+        return 0;
+    }
+    //2,3,2   2,6,1,2   1,2,3,4,17,6
+    //2,6,1,1,2   2,1,2,2,1,2   5,4,3,2,1
+    //打算哪一家
     public int rob(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i=0;i<nums.length;i++){
-            map.put(i,nums[i]);
+        int prevMax = 0;
+        int currMax = 0;
+        for (int x : nums) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + x, currMax);
+            prevMax = temp;
         }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-        int j=list.size()-2;
-        int sum=list.get(list.size()-1).getValue();
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(list.get(list.size()-1).getKey());
-        while (j>=0){
-            boolean isNbr = false;
-            for(int i=0;i<list1.size();i++){
-                if(Math.abs(list.get(j).getKey()-list1.get(i))==1){
-                    isNbr = true;
-                    break;
-                }
-            }
-            if(isNbr){
-                j--;
-            }else{
-                if(sum+list.get(j).getValue()<0){
-                    return Integer.MAX_VALUE;
-                }else{
-                    sum+=list.get(j).getValue();
-                    list1.add(list.get(j).getKey());
-                    j--;
-                }
-            }
-        }
-        return sum;
+        return currMax;
     }
 }
