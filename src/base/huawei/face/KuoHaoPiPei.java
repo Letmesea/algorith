1f969270fa56f3fc00121a277fb9b5b961ac9d20
Letmesea <LIANGZHONGHAI3@ceair.com>
2020-03-31 20:42:22 +0800

    括号匹配

---
+++ src/base/huawei/face/KuoHaoPiPei.java
@@ -0,0 +1,36 @@
+package base.huawei.face;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class KuoHaoPiPei {
+    public static void main(String[] args){
+        Stack<Character> stack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()){
+            String str = sc.nextLine();
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)=='('||str.charAt(i)=='['||str.charAt(i)=='{'){
+                    stack.push(str.charAt(i));
+                }
+                if(str.charAt(i)==')'||str.charAt(i)==']'||str.charAt(i)=='}'){
+                    if(stack.isEmpty()){
+                        System.out.println(false);
+                        break;
+                    }
+                    char c = stack.peek();
+                    char ck = str.charAt(i);
+
+                    if(c=='('&&ck==')'||c=='['&&ck==']'||c=='{'&&ck=='}'){
+                        stack.pop();
+                    }
+                }
+            }
+            if(stack.isEmpty()){
+                System.out.println(true);
+            }else{
+                System.out.println(false);
+            }
+        }
+    }
+}
