a1417b8acacf45899e6d5c05b14a309ed129140c
Letmesea <LIANGZHONGHAI3@ceair.com>
2020-04-04 18:05:22 +0800

    华为面试

---
+++ src/base/huawei/JuZiZhongDanCiFanZhuan.java
@@ -0,0 +1,37 @@
+package base.huawei;
+
+import java.util.Scanner;
+//MyNameIsTom
+//yMemaNsImoT
+//MMyN
+//MMNy
+//LowLL
+//woLLL
+/**
+ * 句子中单词翻转
+ */
+public class JuZiZhongDanCiFanZhuan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            if(str.length()<2){
+                System.out.println(str);
+                continue;
+            }
+            int i=0,j=1;
+            for(;j<str.length();){
+                if(str.charAt(j)>='A'&&str.charAt(j)<='Z'){
+                    StringBuilder stringBuilder = new StringBuilder(str.substring(i,j));
+                    System.out.print(stringBuilder.reverse().toString());
+                    i=j;j++;
+                }else{
+                    j++;
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder(str.substring(i,j));
+            System.out.print(stringBuilder.reverse().toString());
+            System.out.println();
+        }
+    }
+}
